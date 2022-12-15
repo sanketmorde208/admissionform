a8ca689164dd2193cf38c272ba3bebea1bc760d1
Sanket morde <120240577+sanketmorde208@users.noreply.github.com>
2022-12-15 19:42:30 +0530

    Add files via upload

---
+++ connection1.php
@@ -0,0 +1,16 @@
+<?php
+ error_reporting('E_ALL'); 
+ $host = 'localhost';
+ $user = 'root';
+ $dbpass = '';
+ $conMysqli = mysql_connect($host, $user, $dbpass);
+ if(!$conMysqli){ 
+  //echo "connection not successfully";
+ }else{
+ // echo "Connected";
+ }
+  
+ $softdbname ="admission";
+  
+ mysql_select_db($softdbname);  
+?>
